193b8aeb6fb570f83c6b09f3c506e30bdbd27073
Upinder <upindersangha01@gmail.com>
2020-04-03 03:14:46 +0530

    Project complete

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
 	username: String,
 	password: String,
-	googleId:String
+	googleId: String,
+	secret:String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -75,7 +76,6 @@ app.get('/auth/google',
 app.get('/auth/google/secrets',
 	passport.authenticate('google', { failureRedirect: '/login' }),
 	function (req, res) {
-		// Successful authentication, redirect home.
 		res.redirect('/secrets');
 	});
 
@@ -124,12 +124,38 @@ app.route("/login")
 	});
 
 app.get("/secrets", function (req, res) {
+	User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+		if (err)
+			console.log(err);
+		else
+			if (foundUsers)
+				res.render("secrets", { usersWithSecrets: foundUsers });
+	});
+});
+
+
+app.get("/submit", function (req, res) {
 	if (req.isAuthenticated()) {
-		res.render("secrets");
+		res.render("submit");
 	}
 	else {
 		res.redirect("/login");
 	}
+});
+app.post("/submit", function (req, res) {
+	const submittedSecret = req.body.secret;
+	User.findById(req.user.id, function (err, foundUser) {
+		if (err)
+			console.log(err);
+		else {
+			if (foundUser) {
+				foundUser.secret = submittedSecret;
+				foundUser.save(function () {
+					res.redirect("/secrets");
+				});
+			}
+		}
+	});
 });
 
 app.get("/logout", function (req, res) {
